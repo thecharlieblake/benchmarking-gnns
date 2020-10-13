60ebd6fb48522557648970a48f59deeeb464565f
Charles Blake <ms19cb@woma.cs.ox.ac.uk>
2020-10-13 16:30:19 +0100

    Add docker build changes

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,1 @@
-if [ "$1" = 'cu101' ] || [ "$1" = 'cpu' ]; then
-    docker build --build-arg uid=$UID --build-arg user=$USER --build-arg
-    cuda=$1 -t "ms19cb-gnn-freeze-$1" .
-else
-    echo "Invalid cpu/cuda value: $1"
-fi
+docker build -t cuda-101-conda-graph .
--- docker_run.sh
+++ docker_run.sh
@@ -1,12 +1,1 @@
-NV_GPU="$1" nvidia-docker run -it -v `pwd`:/ms19cb/gnn-freeze:rw gnn-freeze
-docker run --runtime=nvidia -it -v `pwd`:/ms19cb/gnn-freeze:rw --hostname
-$HOSTNAME --workdir /ms19cb/gnn-freeze/nervenet3/scripts gnn-freeze
-docker run --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=0 -it -v
-`pwd`:/ms19cb/gnn-freeze:rw gnn-freeze
-
-if [ "$1" = 'cu101' ] || [ "$1" = 'cpu' ]; then
-    docker run -it -v `pwd`:/ms19cb/gnn-freeze:rw --hostname $HOSTNAME
-    --workdir /ms19cb/gnn-freeze/nervenet3/scripts "gnn-freeze-ms19cb-$1"
-else
-    echo "Invalid cpu/cuda value: $1"
-fi
+docker run --gpus all -v `pwd`:/ms19cb/gnn-freeze:rw --hostname $HOSTNAME --workdir /ms19cb/gnn-freeze/ -it cuda-101-conda-graph:latest
